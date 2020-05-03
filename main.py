56e02e134d70684d42eb50aee065fb00bd361d04
Jacob Fisher <48258052+jcbfshr@users.noreply.github.com>
2020-05-03 20:57:14 +0100

    fix/progress-tracker
    Decyrption module now uses the progress tracker

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import cipher,store
-from progress.bar import ShadyBar
+import cipher,store,progressbar
 
 mode = str(input("Encrypt (y/n): "))[0].lower()
 
 if mode == "y":
     no_rotors = int(input("Number of rotors: "))
-    print(f"Use key {cipher.encrypt(input('Plaintext: '),no_rotors)}.rotors to encrypt")
+    print(f"Use key {cipher.encrypt(input('Plaintext: '),no_rotors)}.rotors to decrypt")
 elif mode == "n":
     rotor_settings = store.load(str(input("Rotor settings reference (35 chars): "))[:34])
     rotors = []
 
-    print("Setting rotors . . .")
+    widgets=[
+        ' [', progressbar.Timer(), '] ',
+        progressbar.Bar(marker="█"),
+        ' (', progressbar.ETA(), ') ',
+    ]
 
-    for i in range(len(rotor_settings)):
+    for i in progressbar.progressbar(range(len(rotor_settings)),widgets=widgets):
         rotor = rotor_settings[str(i)]
         if rotor[0] == "spacer":
             rotors.append(cipher.Spacer(i,rotor[1]))
